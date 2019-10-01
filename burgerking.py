7ef027288d2b47cd1d2ec9972016fa106c6662cf
안석준 <sjahn@ex-em.com>
2019-10-01 11:04:55 +0900

    py2 -> py3 수정 및 알고리즘 개선

--- burgerking.py
+++ burgerking.py
@@ -1,127 +1,88 @@
-#-*- coding: utf-8 -*-
-import urllib
-import urllib2
-import cookielib
 from bs4 import BeautifulSoup
+from urllib.request import urlopen, HTTPCookieProcessor, build_opener, install_opener
+from urllib.parse import urlencode
+import http.cookiejar
+import sys
+import random
+import json
+import time
 
-#Returns input_name, input_alias, input_choices
-def parse_input_item(html_str):
-	_soup = BeautifulSoup(html_str, "html.parser")
-	_input_list = _soup.select('input')
-	_input_name = []
-	_input_alias = {}
-	_input_choices = {}
+"""
+returns tuple of (action url, parameter dict)
+"""
+def parse(text):
+    param_dict = {}
+    soup = BeautifulSoup(text, 'html.parser')
+    for input_item in soup.find_all('input'):
+        name = input_item.get("name", None)
+        value = input_item.get("value", None)
+        if name in param_dict and name is not None:
+            param_dict[name].append(value)
+        elif name is not None:
+            param_dict[name] = [value]
+    action = soup.find('form').get('action', None)
+    if param_dict.get('JavaScriptEnabled') != None:
+            param_dict['JavaScriptEnabled'] = '1'
+    return (action, param_dict)
 
-	for eachinput in _input_list:
-		try:
-			_input_name.index(eachinput.get('name'))
-		except ValueError:
-			_input_name.append(eachinput.get('name'))
-			_input_alias[eachinput.get('name')] = [eachinput.get('alt')]
-			_input_choices[eachinput.get('name')] = [eachinput.get('value')]
-		else:
-			_input_alias[eachinput.get('name')].append(eachinput.get('alt'))
-			_input_choices[eachinput.get('name')].append(eachinput.get('value'))
+def select_option(param_dict):
+    for key in param_dict:
+        count = len(param_dict[key])
+        if count > 0:
+            param_dict[key] = param_dict[key][random.randrange(0,count)]
 
-	return _input_name, _input_alias, _input_choices
-		
+def parse_code(text):
+    soup = BeautifulSoup(text, 'html.parser')
+    return soup.select('p.ValCode')[0].text
+
+baseURL = "https://kor.tellburgerking.com/"
+
+if __name__ == "__main__":
+    cj = http.cookiejar.CookieJar()
+    opener = build_opener(HTTPCookieProcessor(cj))
+    install_opener(opener)
+    html = urlopen(baseURL).read()
+    a, d = parse(html)
+    select_option(d)
+    post_data = urlencode(d).encode('utf-8')
+    html = urlopen(baseURL + a, data=post_data).read().decode('utf-8')
+
+    a, d = parse(html)
+    select_option(d)
+    
+    #Input burgerking code
+    print ('버거킹 코드 입력(-입력, 공백 없이):')
+    while True:
+        code = input()
+        if len(code) == 16:
+            break
+        else:
+            print ('코드 길이가 안맞습니다!')
+
+    start_time = time.time()
+    
+    #Make HTTP POST data
+    d.update({
+        'CN1': code[0:3],
+        'CN2': code[3:6],
+        'CN3': code[6:9],
+        'CN4': code[9:12],
+        'CN5': code[12:15],
+        'CN6': code[15],
+    })
+
+    while True:
+        post_data = urlencode(d).encode('utf-8')
+        html = urlopen(
+            baseURL + a, data=post_data).read().decode('utf-8')
+        a, d = parse(html)
+        select_option(d)
+        if d == {}:
+            break
+    
+    end_time = time.time()
+    print ("%s sec elapsed" % (end_time - start_time))
+
+    print(parse_code(html))
 
 
-
-cj = cookielib.CookieJar()
-opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-urllib2.install_opener(opener)
-
-#Find form action link
-html = urllib2.urlopen('https://kor.tellburgerking.com/Index.aspx?c=330406')
-soup = BeautifulSoup(html, "html.parser")
-form = soup.select('form')[0]['action']
-
-#Make HTTP POST data
-payload = {
-	'JavaScriptEnabled' : '1',
-	'FIP': 'True',
-	'AcceptCookies' : 'Y',
-	'NextButton' : u'계속'
-}
-data = urllib.urlencode(payload)
-
-#Send HTTP POST request
-req = urllib2.Request('https://kor.tellburgerking.com/' + form, data)
-html = urllib2.urlopen(req)
-soup = BeautifulSoup(html, "html.parser")
-form = soup.select('form')[0]['action']
-
-#Input burgerking code
-print '버거킹 코드 입력(-입력, 공백 없이):',
-while True:
-	code = raw_input()
-	if len(code) == 16:
-		break
-	else:
-		print '코드 길이가 안맞습니다!'
-
-#Make HTTP POST data
-payload = {
-	'JavaScriptEnabled' : '1',
-	'FIP' : 'True',
-	'CN1' : code[0:2],
-	'CN2' : code[3:5],
-	'CN3' : code[6:8],
-	'CN4' : code[9:11],
-	'CN5' : code[12:14],
-	'CN6' : code[15],
-	'NextButton' : u'계속'
-}
-data = urllib.urlencode(payload)
-
-#Send HTTP POST request
-req = urllib2.Request('https://kor.tellburgerking.com/' + form, data)
-html = urllib2.urlopen(req)
-soup = BeautifulSoup(html, "html.parser")
-form = soup.select('form')[0]['action']
-
-#주문유형?
-#Make HTTP POST data
-payload = {
-	'R001000': '1', #내점 : 1, #takeout : 2
-	'IoNF' : '2',
-	'PostedFNS' : 'S001000|R001000'
-}
-data = urllib.urlencode(payload)
-
-#Send HTTP POST request
-req = urllib2.Request('https://kor.tellburgerking.com/' + form, data)
-html = urllib2.urlopen(req)
-soup = BeautifulSoup(html, "html.parser")
-form = soup.select('form')[0]['action']
-
-#방문자수
-#Make HTTP POST data
-payload = {
-	'R002000': '1', #1, 2, 3, 4 : 인원수
-	'IoNF' : '2',
-	'PostedFNS' : 'R002000'
-}
-data = urllib.urlencode(payload)
-
-#Send HTTP POST request
-req = urllib2.Request('https://kor.tellburgerking.com/' + form, data)
-html = urllib2.urlopen(req)
-soup = BeautifulSoup(html, "html.parser")
-form = soup.select('form')[0]['action']
-
-#12세 미만?
-#Make HTTP POST data
-payload = {
-	'R003000': '1', #1:Y, 2:N
-	'IoNF' : '2',
-	'PostedFNS' : 'R003000'
-}
-data = urllib.urlencode(payload)
-
-#Send HTTP POST request
-req = urllib2.Request('https://kor.tellburgerking.com/' + form, data)
-html = urllib2.urlopen(req)
-soup = BeautifulSoup(html, "html.parser")
-form = soup.select('form')[0]['action']
